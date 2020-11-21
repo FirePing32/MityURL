4bc838539b884b6110bce63a8bb394bc376dac0e
Prakhar Gurunani <prakhargurunani@gmail.com>
2020-11-21 14:06:36 +0530

    Setup actions to build app

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import firebase from 'firebase';
 import Modal from 'react-native-modal';
 
 var config = {
-    apiKey: "process.env.API_KEY",
-    authDomain: "process.env.AUTH_DOMAIN",
-    databaseURL: "process.env.DATABASE_URL",
-    projectId: "process.env.PROJECT_ID",
-    storageBucket: "process.env.STORAGE_BUCKET",
-    messagingSenderId: "process.env.MESSAGING_SENDER_ID",
-    appId: "process.env.APP_ID",
-    measurementId: "process.env.MEASUREMENT_ID"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
@@ -57,7 +57,7 @@ export default class App extends React.Component {
 
   push_data = () => {
     firebase.database().ref('urls/').child(this.random_key()).set(this.state.url);
-    this.state.redirect_url = "https://" + process.env.MY-APP-NAME + ".vercel.app/" + this.state.url_id;
+    this.state.redirect_url = "https://" + process.env.MY_APP_NAME + ".vercel.app/" + this.state.url_id;
     this.textInput.clear()
     this.toggleModal()
   }
